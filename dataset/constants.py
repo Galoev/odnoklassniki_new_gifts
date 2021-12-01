dc8109587fbb1d91eb766c5446890211b120de60
Galoev <galoyev.ilkin@gmail.com>
2021-12-02 00:34:01 +0300

    add new constants

--- dataset/constants.py
+++ dataset/constants.py
@@ -2,3 +2,5 @@ from pathlib import Path
 
 NYU_V2_URL = "http://horatio.cs.nyu.edu/mit/silberman/nyu_depth_v2/nyu_depth_v2_labeled.mat"
 PATH_TO_NYU = Path("/Volumes/TMP_Storage/Datasets/NYUv2")
+RAW_NYU_V2_URL = "http://datasets.lids.mit.edu/fastdepth/data/nyudepthv2.tar.gz"
+RAW_NYU_V2_FILE_NAME = "nyudepthv2.tar.gz"
