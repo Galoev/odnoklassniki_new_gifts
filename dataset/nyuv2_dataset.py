b18c1e3cf90621e0d845ef67b7d63c4c12c59fa9
Galoev <galoyev.ilkin@gmail.com>
2021-12-22 17:32:17 +0300

    add small fix datasets 3

--- dataset/nyuv2_dataset.py
+++ dataset/nyuv2_dataset.py
@@ -6,9 +6,16 @@ from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import torchvision.transforms as T
 from torchvision.transforms import functional
+from torchvision import transforms
 
 output_height=256
 output_width=256
+
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((output_height, output_width)),
+    transforms.ToTensor(),
+])
 
 class DatasetNYUv2(Dataset):
     PATH_TO_NYU = Path("/Volumes/TMP_Storage/Datasets/NYUv2/datasets.lids.mit.edu/_nyudepthv2")
@@ -42,14 +49,14 @@ class DatasetNYUv2(Dataset):
         # rgb_img = rgb_img.transpose((1, 2, 0))
         rgb_img = torch.tensor(rgb_img)
         rgb_img = rgb_img.float()
-        rgb_img = functional.resize(rgb_img, size=(output_height, output_width))
+        rgb_img = transform(rgb_img)
         
         depth_img = np.array(h5_data['depth'][:])
         depth_img = np.reshape(depth_img, (1, depth_img.shape[0], depth_img.shape[1]))
         depth_img = torch.tensor(depth_img)
         depth_img = depth_img.float()
         depth_img = torch.clamp(depth_img, min=0, max=1)
-        depth_img = functional.resize(depth_img, size=(output_height, output_width))
+        depth_img = transform(depth_img)
         
         # depth_img /= 10
         # depth_img *= 255
