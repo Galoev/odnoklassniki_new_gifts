21736b9a3c35e20868c677ec6f09f8e66b01eedb
Galoev <galoyev.ilkin@gmail.com>
2021-12-26 03:43:23 +0300

    add to inference scan all imgs in folder and eval them

--- predict_img.py
+++ predict_img.py
@@ -3,9 +3,10 @@ import cv2
 import torch
 from network import ResnetUnetHybrid
 import image_utils
+from pathlib import Path
 
 
-def predict_img(img_path):
+def predict_img(args):
     """Inference a single image."""
     # switch to CUDA device if possible
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -13,35 +14,45 @@ def predict_img(img_path):
 
     # load model
     print('Loading model...')
-    model = ResnetUnetHybrid.load_pretrained(device=device)
+    model = ResnetUnetHybrid.load_pretrained(device=device, load_path=args.model_path)
     model.eval()
 
-    # load image
-    img = cv2.imread(img_path)[..., ::-1]
-    img = image_utils.scale_image(img)
-    img = image_utils.center_crop(img)
-    inp = image_utils.img_transform(img)
-    inp = inp[None, :, :, :].to(device)
+    images_path = Path(args.imgs_path)
+    output_folder = Path(args.imgs_path + "/output/")
+    output_folder.mkdir(parents=True, exist_ok=True)
 
-    # inference
-    print('Running the image through the network...')
-    output = model(inp)
+    for img_path in images_path.glob('*'):
+        if img_path.is_dir():
+            continue
+        # load image
+        img = cv2.imread(img_path)[..., ::-1]
+        img = image_utils.scale_image(img)
+        img = image_utils.center_crop(img)
+        inp = image_utils.img_transform(img)
+        inp = inp[None, :, :, :].to(device)
 
-    # transform and plot the results
-    output = output.cpu()[0].data.numpy()
-    image_utils.show_img_and_pred(img, output)
+        # inference
+        print('Running the image through the network...')
+        output = model(inp)
+
+        # transform and plot the results
+        output = output.cpu()[0].data.numpy()
+        image_utils.show_img_and_pred(img, output)
+        image_utils.save_pred(output, output_folder/img_path.name)
+    
 
 
 def get_arguments():
     """Get command line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--img_path', required=True, type=str, help='Path to the input image.')
+    parser.add_argument('--imgs_path', required=True, type=str, help='Path to the input image.')
+    parser.add_argument('--model_path', required=True, type=str, help='Path to the model')
     return parser.parse_args()
 
 
 def main():
     args = get_arguments()
-    predict_img(args.img_path)
+    predict_img(args)
 
 
 if __name__ == '__main__':
